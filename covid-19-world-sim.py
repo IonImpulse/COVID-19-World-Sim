d90302c3ed2d8199717d9787ed7598cd6a859e88
Ethan Vazquez <edv121@outlook.com>
2020-04-27 09:33:02 -0700

    update

--- covid-19-world-sim.py
+++ covid-19-world-sim.py
@@ -9,6 +9,22 @@ import math
 This is a python program to 
 '''
 
+def haversine(lon_lat1, lon_lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(math.radians, list(lon_lat1) + list(lon_lat2))
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a)) 
+    # Radius of earth in kilometers is 6371
+    km = 6371* c
+    return km
+
 
 class Country :
     '''
@@ -22,14 +38,14 @@ class Country :
 
     '''
 
-    def __init__(self, iso2, cities, current_order, gdp_captia) :
+    def __init__(self, iso2, cities, current_order, gdp_captia, beds_per_1000) :
         self.iso2 = iso2        
         self.current_order = current_order
         self.gdp_captia = gdp_captia
-        
+        self.beds_per_1000 = beds_per_1000
         self.cities = self.create_cities(cities)
         
-
+        self.ledger = pd.DataFrame(columns=list(cities.keys()))
 
     def create_cities(self, cities) :
         cities_dict = {}
@@ -39,9 +55,12 @@ class Country :
             
             #Set up lat/lon as tuple
             temp_lat_lon = (float(row["Latitude"]), float(row["Longitude"]))
-
+            
+            #Calculate number of hopsital beds per city, which changes with pop. and gdp
+            hospital_beds_aval = row["Population"]/1000 * self.beds_per_1000
+            
             #Create city object and place it in dict by name
-            cities_dict[row["City"]] = City(row["Population"], 0, 0, 0, temp_lat_lon, self.current_order, self.gdp_captia)
+            cities_dict[row["City"]] = City(row["Population"], 0, 0, 0, temp_lat_lon, self.current_order, self.gdp_captia, hospital_beds_aval)
 
         return cities_dict
 
@@ -56,13 +75,13 @@ class City :
         immune,
         deceased,
         lat/lon,
-    
+        hospital beds available
     Inherits:
         current order (free, shelter-in-place recommended, shelter-in-place mandatory),
         gdp-per-capita
     '''
 
-    def __init__(self, population, infected, recovered, deceased, lat_lon, current_order, gdp_per_captia) :
+    def __init__(self, population, infected, recovered, deceased, lat_lon, current_order, gdp_per_captia, hospital_beds_aval) :
         self.population = population
         self.infected = infected
         self.recovered = recovered
@@ -70,10 +89,11 @@ class City :
         self.lat_lon = lat_lon
         self.current_order = current_order
         self.gdp_per_capita = gdp_per_captia
+        self.hospital_beds_aval = hospital_beds_aval
 
 
 
-def create_countries_threaded(name_lookup, cities_database, gdp_captia, iso2_iso3, output_dict) : 
+def create_countries_threaded(name_lookup, cities_database, gdp_captia, iso2_iso3, output_dict, beds_per_1000) : 
     temp_countries_dict = {}
     
     #Iterate over the ISO2 to name lookup for convenience
@@ -86,10 +106,17 @@ def create_countries_threaded(name_lookup, cities_database, gdp_captia, iso2_iso
         if len(temp_cities_df.index) > 0 :
                     
             try :
+                #Only lookup iso3 once
+                iso3 = iso2_iso3[iso2]
+
                 #Get gdp by using ISO2 to ISO3 dict and looking up in the GDP JSON file, as GDP is using ISO3
-                temp_gdp_captia = gdp_captia[iso2_iso3[iso2]]
+                temp_gdp_captia = gdp_captia[iso3]
+
+                #Get beds per 100 by looking up iso3 code for JSON file
+                temp_beds_per_1000 = beds_per_1000[iso3]
+
                 #Create country object and place it in a dict by name
-                temp_countries_dict[name_lookup[iso2]] = Country(iso2, temp_cities_df, "None", temp_gdp_captia)
+                temp_countries_dict[name_lookup[iso2]] = Country(iso2, temp_cities_df, "None", temp_gdp_captia, temp_beds_per_1000)
                     
             except Exception as e :
                 print("Skipping", iso2)
@@ -106,14 +133,33 @@ class Virus_Simulator :
     '''
     Main class for this project
     Has many different function, with the main function being iterate_day()  
+    infection_rate = 0-1
+    percent_social_distancing = 0-1
+    government_action_timing = 0-1
     '''
     
-    def __init__(self) :
+    def __init__(self, infection_rate, percent_social_distancing, government_action_timing, time_in_hospital, hospitalization_rate, mortality_rate) :
         '''
-        Do nothing in __init__ as it may be necessary to generate data to load in other functions first
-        Use method load_data() when ready to start
+        Sets paramaters to control simulation
         '''
-        pass
+
+        #How likely an encounter is to spread the virus
+        self.infection_rate = infection_rate
+
+        #How many people will social distance once order is given
+        self.percent_social_distancing = percent_social_distancing
+
+        #When governments will impose shelter in place order (percent of pop infected)
+        self.government_action_timing = government_action_timing
+
+        #How long people spend in hospital before dying or recovering
+        self.time_in_hospital = time_in_hospital
+
+        #What percentage of cases need hospitalization
+        self.hospitalization_rate = hospitalization_rate
+
+        #Rate of death. Doubled when not in hospitals.
+        self.mortality_rate = mortality_rate
 
     def filter_raw_cities_datafile(self, path_to_file) :
         '''
@@ -188,6 +234,30 @@ class Virus_Simulator :
         with open("Data\\gdp-capita.json", 'w') as target:
             json.dump(gdp_data, target)
 
+    def filter_raw_beds_datafile(self, path_to_file) :
+        #Create dict to dump to JSON
+        beds_data = {}
+        
+        #Don't use pandas, as it is only about 200 lines
+        with open(path_to_file, 'r') as target :
+            csv_reader = csv.reader(target)
+            raw_bed_data = []
+
+            for row in csv_reader :
+                raw_bed_data.append(row)
+        
+        del raw_bed_data[0]
+
+        for row in raw_bed_data :
+            temp_list = [i for i in row[4:] if i.replace(" ", "") != ""]
+            
+            if len(temp_list) > 0 :
+                beds_data[row[1]] = float(temp_list[-1])
+        
+        #Dump the dict to JSON file, to be read later as dict again.
+        with open("Data\\bed-per-1000.json", 'w') as target:
+            json.dump(beds_data, target)
+
     def load_data(self) :
         '''
         Loads cities database and JSON lookup files into memory
@@ -222,9 +292,9 @@ class Virus_Simulator :
             with open("Data\\gdp-capita.json") as target :
                 self.gdp_captia = json.load(target)
 
-            #Generated age/key table
-            with open("Data\\age-key.json") as target :
-                self.age_key = json.load(target)
+            #Generated beds per 1000 table
+            with open("Data\\beds-per-1000.json") as target :
+                self.beds_per_1000 = json.load(target)
 
         except Exception as e :
             print(e)
@@ -252,11 +322,8 @@ class Virus_Simulator :
             
             current_thread += 1
 
-            if current_thread > 7 :
+            if current_thread == self.num_of_threads :
                 current_thread = 0
-
-        print(temp_cities_list)
-
 
         #Create dict to be filled with country objects
         manager = multiprocessing.Manager()
@@ -268,7 +335,7 @@ class Virus_Simulator :
         for i in range(self.num_of_threads) :
 
             #Create process with create_countries_threaded as target
-            p = multiprocessing.Process(target=create_countries_threaded, args=(temp_cities_list[i], self.cities_database, self.gdp_captia, self.iso2_iso3, self.countries_dict))
+            p = multiprocessing.Process(target=create_countries_threaded, args=(temp_cities_list[i], self.cities_database, self.gdp_captia, self.iso2_iso3, self.countries_dict, self.beds_per_1000))
             
             temp_process_list.append(p)
 
@@ -277,10 +344,17 @@ class Virus_Simulator :
         for process in temp_process_list :
             process.join()
 
-
+        print("Finished setting up " + str(len(self.countries_dict)) + " countries!")
+        
         #Setup dataframe for storing current state of everything, for easier access then looking up all values
         #This allows us to pass it as a value in the iterate_day_threaded function
-        self.current_global_state = pd.Dataframe()
+        
+        list_of_countries = sorted(self.countries_dict.keys())
+
+        #Create a dataframe as a ledger to keep track of everything, mainly as a way to easily save the state of sim and export to a graph
+        self.current_global_state = pd.DataFrame(columns=list_of_countries)
+
+        print(self.current_global_state)
 
 
 
@@ -306,9 +380,10 @@ class Virus_Simulator :
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     #Create simulator object
-    sim = Virus_Simulator()
+    #infection_rate, percent_social_distancing, government_action_timing, time_in_hospital, hospitalization_rate, mortality_rate
+    sim = Virus_Simulator(.2, .7, .05, 14, .12, .2)
 
     '''
     Uncomment to create datafiles if missing
@@ -316,7 +391,7 @@ if __name__ == '__main__':
     '''
     #sim.filter_raw_cities_datafile("Data\\worldcitiespop.csv)
     #sim.filter_raw_gdp_datafile("Data\\raw_gdp_capita.csv")
-
+    #sim.filter_raw_beds_datafile("Data\\beds.csv")
     
     #Load all data
     sim.load_data()
